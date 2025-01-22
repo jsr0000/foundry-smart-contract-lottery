5f185a721c26ed0b0e3cb8da226e5f55f44987d6
jsr0000 <josh.regnart@gmail.com>
2025-01-22 14:19:39 +0000

    enterRaffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,25 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
@@ -10,19 +32,33 @@ pragma solidity 0.8.19;
  */
 
 contract Raffle {
-
+    // Error messages
+    error Raffle__SendMoreToEnterRaffle();
+    // State variables
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    // Events
+    event RaffleEntered(address indexed player);
 
-    constructor(uint256 entranceFee) {
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
     }
 
-    function enterRaffle() public payable{
-        // Enter the raffle
+    function enterRaffle() external payable{
+        // require(msg.value >= i_entranceFee, "Not enough funds to enter the raffle");
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+        if(msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {
-        // Pick the winner
+    function pickWinner() external {
+        
     }
 
     // Get the entrance fee
