d6ea0be0cebf2056fe7fe12d84fc85a13956c0cd
jsr0000 <josh.regnart@gmail.com>
2025-01-28 15:28:37 +0000

    Add VRF mock configuration for local network testing

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
     uint256 public constant SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
+
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+    int256 public MOCK_WEI_PER_UNIT_LINK = 4e15;
 }
 
 contract HelperConfig is Script, CodeConstants {
@@ -55,5 +60,26 @@ contract HelperConfig is Script, CodeConstants {
         if (localNetworkConfig.vrfCoordinatorV2 != address(0)) {
             return localNetworkConfig;
         }
+
+        vm.startBroadcast();
+
+        VRFCoordinatorV2_5Mock vrfCoordinator = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UNIT_LINK
+        );
+
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callbackGasLimit: 500000,
+            vrfCoordinatorV2: address(vrfCoordinatorMock),
+            subscriptionId: 0
+        });
+
+        return localNetworkConfig;
     }
 }
