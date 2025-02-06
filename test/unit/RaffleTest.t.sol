c900f9d28664f28dede0740b09fe0f3a5661c16a
jsr0000 <josh.regnart@gmail.com>
2025-02-06 08:17:45 +0000

    Add performUpkeep tests for Raffle contract validation

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -90,4 +90,29 @@ contract RaffleTest is Test {
 
     // testCheckUpkeepReturnsFalseIfEnoughTimeHasntPassed()
     //testCheckUpkeepReturnsTrueWhenParametersAreGood()
+
+    function testPerformUpkeepCanOnlyBeCalledIfCheckUpkeepIsTrue() public {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        raffle.performUpkeep("");
+    }
+
+    function testPerformUpkeepRevertsIfCheckUpkeepIsFalse() public {
+        uint256 currentBalance = 0;
+        uint256 numPlayers = 0;
+        Raffle.RaffleState rState = raffle.getRaffleState();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Raffle.Raffle__UpkeepNotNeeded.selector,
+                currentBalance,
+                numPlayers,
+                rState
+            )
+        );
+        raffle.performUpkeep("");
+    }
 }
